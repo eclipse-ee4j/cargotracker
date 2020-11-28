ce86e7146f8f2dfc42d6db979ec19cab36c011b6
Reza Rahman <reza_rahman@lycos.com>
2020-11-28 18:37:20 -0500

    Minor review updates.

--- src/main/java/org/eclipse/cargotracker/infrastructure/persistence/jpa/JpaVoyageRepository.java
+++ src/main/java/org/eclipse/cargotracker/infrastructure/persistence/jpa/JpaVoyageRepository.java
@@ -1,34 +1,33 @@
 package org.eclipse.cargotracker.infrastructure.persistence.jpa;
 
-import org.eclipse.cargotracker.domain.model.voyage.Voyage;
-import org.eclipse.cargotracker.domain.model.voyage.VoyageNumber;
-import org.eclipse.cargotracker.domain.model.voyage.VoyageRepository;
+import java.io.Serializable;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.io.Serializable;
-import java.util.List;
+
+import org.eclipse.cargotracker.domain.model.voyage.Voyage;
+import org.eclipse.cargotracker.domain.model.voyage.VoyageNumber;
+import org.eclipse.cargotracker.domain.model.voyage.VoyageRepository;
 
 @ApplicationScoped
 public class JpaVoyageRepository implements VoyageRepository, Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @PersistenceContext
-    private EntityManager entityManager;
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    public Voyage find(VoyageNumber voyageNumber) {
-        return entityManager
-                .createNamedQuery("Voyage.findByVoyageNumber", Voyage.class)
-                .setParameter("voyageNumber", voyageNumber).getSingleResult();
-    }
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    @Override
-    public List<Voyage> findAll() {
-        return entityManager.createNamedQuery("Voyage.findAll", Voyage.class)
-                .getResultList();
-    }
+	@Override
+	public Voyage find(VoyageNumber voyageNumber) {
+		return entityManager.createNamedQuery("Voyage.findByVoyageNumber", Voyage.class)
+				.setParameter("voyageNumber", voyageNumber).getSingleResult();
+	}
 
+	@Override
+	public List<Voyage> findAll() {
+		return entityManager.createNamedQuery("Voyage.findAll", Voyage.class).getResultList();
+	}
 
 }
