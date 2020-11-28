71b1856fa3665db2adfd2e1a02b66cb146de8f66
Reza Rahman <reza_rahman@lycos.com>
2020-11-28 16:42:06 -0500

    Minor review changes.

--- src/main/java/org/eclipse/cargotracker/application/CargoInspectionService.java
+++ src/main/java/org/eclipse/cargotracker/application/CargoInspectionService.java
@@ -1,16 +1,15 @@
 package org.eclipse.cargotracker.application;
 
-import org.eclipse.cargotracker.domain.model.cargo.TrackingId;
-
 import javax.validation.constraints.NotNull;
+
+import org.eclipse.cargotracker.domain.model.cargo.TrackingId;
 
 public interface CargoInspectionService {
 
-    /**
-     * Inspect cargo and send relevant notifications to interested parties, for
-     * example if a cargo has been misdirected, or unloaded at the final
-     * destination.
-     */
-    public void inspectCargo(
-            @NotNull(message = "Tracking ID is required") TrackingId trackingId);
+	/**
+	 * Inspect cargo and send relevant notifications to interested parties, for
+	 * example if a cargo has been misdirected, or unloaded at the final
+	 * destination.
+	 */
+	public void inspectCargo(@NotNull(message = "Tracking ID is required") TrackingId trackingId);
 }
