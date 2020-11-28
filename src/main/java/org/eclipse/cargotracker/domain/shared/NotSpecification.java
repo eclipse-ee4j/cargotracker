4f6e979defac51d7e3ed67d8d0b4289778a0e1e2
Reza Rahman <reza_rahman@lycos.com>
2020-11-28 18:27:37 -0500

    Minor review updates.

--- src/main/java/org/eclipse/cargotracker/domain/shared/NotSpecification.java
+++ src/main/java/org/eclipse/cargotracker/domain/shared/NotSpecification.java
@@ -6,22 +6,22 @@ package org.eclipse.cargotracker.domain.shared;
  */
 public class NotSpecification<T> extends AbstractSpecification<T> {
 
-    private final Specification<T> spec1;
+	private final Specification<T> spec1;
 
-    /**
-     * Create a new NOT specification based on another spec.
-     *
-     * @param spec1 Specification instance to not.
-     */
-    public NotSpecification(Specification<T> spec1) {
-        this.spec1 = spec1;
-    }
+	/**
+	 * Create a new NOT specification based on another spec.
+	 *
+	 * @param spec1 Specification instance to not.
+	 */
+	public NotSpecification(Specification<T> spec1) {
+		this.spec1 = spec1;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isSatisfiedBy(T t) {
-        return !spec1.isSatisfiedBy(t);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isSatisfiedBy(T t) {
+		return !spec1.isSatisfiedBy(t);
+	}
 }
