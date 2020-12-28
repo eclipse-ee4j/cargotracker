d7af0b2adf9480bde6806c504472c5bb60b2ed1f
Reza Rahman <reza_rahman@lycos.com>
2020-12-28 01:13:56 -0500

    Remove dead code.

--- src/main/java/org/eclipse/cargotracker/application/util/DateUtil.java
+++ src/main/java/org/eclipse/cargotracker/application/util/DateUtil.java
@@ -2,7 +2,6 @@ package org.eclipse.cargotracker.application.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -35,25 +34,4 @@ public class DateUtil {
 		// 03/15/2014 12:00 AM CET
 		return dateTime.substring(dateTime.indexOf(" ") + 1);
 	}
-
-	// compute number of days between today and endDate (both set at midnight)
-	public static long computeDuration(Date endDate) {
-		// SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz
-		// yyyy");
-		Date today = trim(new Date()); // from today
-		long diff = endDate.getTime() - today.getTime();
-		return (diff / (24 * 60 * 60 * 1000)); // in days
-	}
-
-	public static Date trim(Date date) { // set time at midnight since we don't deal with time in the day
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		calendar.set(Calendar.AM_PM, Calendar.AM);
-		calendar.set(Calendar.MILLISECOND, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.HOUR, 0);
-		return calendar.getTime();
-	}
-
 }
--- src/main/java/org/eclipse/cargotracker/application/util/LocationUtil.java
+++ src/main/java/org/eclipse/cargotracker/application/util/LocationUtil.java
@@ -1,11 +1,10 @@
 package org.eclipse.cargotracker.application.util;
 
-import org.eclipse.cargotracker.domain.model.location.Location;
-import org.eclipse.cargotracker.domain.model.location.SampleLocations;
-import org.primefaces.model.map.LatLng;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.eclipse.cargotracker.domain.model.location.Location;
+import org.eclipse.cargotracker.domain.model.location.SampleLocations;
 
 public class LocationUtil {
 
@@ -40,105 +39,4 @@ public class LocationUtil {
 
         return locationsCode;
     }
-
-    public static String getPortCoordinates(String portCode) {
-
-        switch (portCode.toUpperCase()) {
-            case "USCHI":
-                return "41.5, -87.38";
-            case "USDAL":
-                return "32.7767, -96.7970";
-            case "SEGOT":
-                return "57.42, 11.57";
-            case "DEHAM":
-                return "53.33, 9.59";
-            case "CNHGH":
-                return "23.6, 113.14";
-            case "FIHEL":
-                return "60.1015, 25.5715";
-            case "CNHKG":
-                return "22.336104, 114.073471";
-            case "AUMEL":
-                return "-37.50, 144.58";
-            case "USNYC":
-                return "40.4252, -74.0022";
-            case "NLRTM":
-                return "51.55 , 4.24";
-            case "CNSHA":
-                return "31.324062, 121.677776";
-            case "SESTO":
-                return "59.19, 18.3";
-            case "JNTKO":
-                return "35.43 , 139.46";
-            default:
-                return null;
-        }
-    }
-
-    public static String getCode(String city) {
-        switch (city.toUpperCase()) {
-            case "CHICAGO":
-                return "USCHI";
-            case "DALLAS":
-                return "USDAL";
-            case "GOTHENBURG":
-                return "SEGOT";
-            case "HAMBURG":
-                return "DEHAM";
-            case "HANGZHOU":
-                return "CNHGH";
-            case "HELSINKI":
-                return "FIHEL";
-            case "HONG KONG":
-                return "CNHKG";
-            case "MELBOURNE":
-                return "AUMEL";
-            case "NEW YORK":
-                return "USNYC";
-            case "ROTTERFDAM":
-                return "NLRTM";
-            case "SHANGHAI":
-                return "CNSHA";
-            case "STOCKHOLM":
-                return "SESTO";
-            case "TOKYO":
-                return "JNTKO";
-            default:
-                return null;
-        }
-    }
-
-    public static LatLng getCoordinatesForLocation(String portCode) {
-        switch (portCode.toUpperCase()) {
-            case "USCHI":
-                return new LatLng(41.5, -87.38);
-            case "USDAL":
-                return new LatLng(32.7767, -96.7970);
-            case "SEGOT":
-                return new LatLng(57.42, 11.57);
-            case "DEHAM":
-                return new LatLng(53.33, 9.59);
-            case "CNHGH":
-                return new LatLng(23.6, 113.14);
-            case "FIHEL":
-                return new LatLng(60.1015, 25.5715);
-            case "CNHKG":
-                return new LatLng(22.336104, 114.073471);
-            case "AUMEL":
-                return new LatLng(-37.50, 144.58);
-            case "USNYC":
-                return new LatLng(40.4252, -74.0022);
-            case "NLRTM":
-                return new LatLng(51.55, 4.24);
-            case "CNSHA":
-                return new LatLng(31.324062, 121.677776);
-            case "SESTO":
-                return new LatLng(59.19, 18.3);
-            case "JNTKO":
-                return new LatLng(35.43, 139.46);
-            default:
-                return null;
-        }
-    }
-
 }
