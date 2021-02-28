46424c6d8b708528c7ced6f5cee825601ead73e2
Reza Rahman <reza_rahman@lycos.com>
2021-02-28 16:03:45 -0500

    Update FacesConfiguration.java

--- src/main/java/org/eclipse/cargotracker/interfaces/FacesConfiguration.java
+++ src/main/java/org/eclipse/cargotracker/interfaces/FacesConfiguration.java
@@ -2,7 +2,8 @@ package org.eclipse.cargotracker.interfaces;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
 
-@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@FacesConfig(version = Version.JSF_2_3)
 @ApplicationScoped
 public class FacesConfiguration {}
