ac4814e62b8dcf1fdb7ca1eacb1c51f0f0f7c3a3
Akira Kawauchi <kasabuta4@gmail.com>
2021-03-13 18:37:27 +0900

    Fix formatting errors of RealtimeCargoTrackingService
    Signed-off-by: Akira Kawauchi <kasabuta4@gmail.com>

--- src/main/java/org/eclipse/cargotracker/interfaces/booking/sse/RealtimeCargoTrackingService.java
+++ src/main/java/org/eclipse/cargotracker/interfaces/booking/sse/RealtimeCargoTrackingService.java
@@ -45,10 +45,7 @@ public class RealtimeCargoTrackingService {
   @GET
   @Produces(MediaType.SERVER_SENT_EVENTS)
   public void tracking(@Context SseEventSink eventSink) {
-    cargoRepository.findAll()
-      .stream()
-      .map(this::cargoToSseEvent)
-      .forEach(eventSink::send);
+    cargoRepository.findAll().stream().map(this::cargoToSseEvent).forEach(eventSink::send);
     broadcaster.register(eventSink);
     logger.log(Level.INFO, "Sse event sink registered");
   }
@@ -62,46 +59,48 @@ public class RealtimeCargoTrackingService {
   public void onCargoInspected(@ObservesAsync @CargoInspected Cargo cargo) {
     broadcaster.broadcast(cargoToSseEvent(cargo));
   }
-  
+
   private OutboundSseEvent cargoToSseEvent(Cargo cargo) {
-    
-    JsonObject cargoInJson = Json.createObjectBuilder()
-        .add("trackingId", cargo.getTrackingId().getIdString())
-        .add("routingStatus", cargo.getDelivery().getRoutingStatus().toString())
-        .add("misdirected", cargo.getDelivery().isMisdirected())
-        .add("transportStatus", cargo.getDelivery().getTransportStatus().toString())
-        .add("atDestination", cargo.getDelivery().isUnloadedAtDestination())
-        .add("origin", cargo.getOrigin().getUnLocode().getIdString())
-        .add("lastKnownLocation",
-            cargo.getDelivery().getLastKnownLocation().getUnLocode().getIdString())
-        .add("locationCode",
-            cargo.getDelivery().getTransportStatus() == TransportStatus.NOT_RECEIVED
-            ? cargo.getOrigin().getUnLocode().getIdString()
-            : cargo.getDelivery().getLastKnownLocation().getUnLocode().getIdString()
-        )
-        .add("statusCode", statusCode(cargo))
-        .build();
-    
+
+    JsonObject cargoInJson =
+        Json.createObjectBuilder()
+            .add("trackingId", cargo.getTrackingId().getIdString())
+            .add("routingStatus", cargo.getDelivery().getRoutingStatus().toString())
+            .add("misdirected", cargo.getDelivery().isMisdirected())
+            .add("transportStatus", cargo.getDelivery().getTransportStatus().toString())
+            .add("atDestination", cargo.getDelivery().isUnloadedAtDestination())
+            .add("origin", cargo.getOrigin().getUnLocode().getIdString())
+            .add(
+                "lastKnownLocation",
+                cargo.getDelivery().getLastKnownLocation().getUnLocode().getIdString())
+            .add(
+                "locationCode",
+                cargo.getDelivery().getTransportStatus() == TransportStatus.NOT_RECEIVED
+                    ? cargo.getOrigin().getUnLocode().getIdString()
+                    : cargo.getDelivery().getLastKnownLocation().getUnLocode().getIdString())
+            .add("statusCode", statusCode(cargo))
+            .build();
+
     return sse.newEventBuilder()
         .mediaType(MediaType.APPLICATION_JSON_TYPE)
         .data(cargoInJson)
         .build();
   }
-  
+
   private String statusCode(Cargo cargo) {
     RoutingStatus routingStatus = cargo.getDelivery().getRoutingStatus();
     if (routingStatus == RoutingStatus.NOT_ROUTED || routingStatus == RoutingStatus.MISROUTED) {
       return routingStatus.toString();
     }
-    
+
     if (cargo.getDelivery().isMisdirected()) {
       return "MISDIRECTED";
     }
-    
+
     if (cargo.getDelivery().isUnloadedAtDestination()) {
       return "AT_DESTINATION";
     }
-    
+
     return cargo.getDelivery().getTransportStatus().toString();
   }
 }
