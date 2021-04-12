344eb55ef5f89b5ab24a42e78b7c761a1d69df06
Hantsy Bai <hantsy@gmail.com>
2021-04-12 10:18:32 +0800

    chore: restore RestConfiguration
    Signed-off-by: Hantsy Bai <hantsy@gmail.com>

--- src/main/java/org/eclipse/cargotracker/application/util/RestConfiguration.java
+++ src/main/java/org/eclipse/cargotracker/application/util/RestConfiguration.java
@@ -1,5 +1,7 @@
 package org.eclipse.cargotracker.application.util;
 
+import org.glassfish.jersey.server.ServerProperties;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashMap;
@@ -12,11 +14,7 @@ public class RestConfiguration extends Application {
   @Override
   public Map<String, Object> getProperties() {
     Map<String, Object> properties = new HashMap<String, Object>();
-    // properties.put(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
-    // for portable in other application servers.
-    // see:
-    // https://eclipse-ee4j.github.io/jersey.github.io/apidocs/latest/jersey/org/glassfish/jersey/server/ServerProperties.html#BV_SEND_ERROR_IN_RESPONSE
-    properties.put("jersey.config.beanValidation.enableOutputValidationErrorEntity.server", true);
+    properties.put(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
     return properties;
   }
 }
