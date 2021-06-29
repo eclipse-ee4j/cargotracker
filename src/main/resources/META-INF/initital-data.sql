2ef76d02412998f28af210ab55593d8bd3f64aeb
Zheng Chang <zhengchang@microsoft.com>
2021-06-29 17:09:10 +0800

    Configuration changes to enable running CargoTracker on WebLogic Server on Azure VMs or Azure Kubernetes Service
    Author:    Zheng Chang <zhengchang@microsoft.com>
    
    setup WebLogic environment
    
    Improve setting initial sql
    
    update db data source name
    
    add aks profile

--- src/main/resources/META-INF/initital-data.sql
+++ src/main/resources/META-INF/initital-data.sql
@@ -1,2 +1,2 @@
--- It's OK for this to fail if the record already exists.
-INSERT INTO applicationsettings (id, sampleloaded) VALUES (1, FALSE);
+-- Init the sample loading flag, won't update if record exists
+INSERT INTO applicationsettings (id, sampleloaded) VALUES (1, FALSE) ON CONFLICT (id) DO NOTHING;
