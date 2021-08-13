fb9b1c7b7ede6c89abe9b8812beb4f5f5f1f2f5d
Ed Burns <edburns@microsoft.com>
2021-08-13 19:09:10 -0400

    On branch cargotracker-wls-aks-dd-1343682 Try to make the pipeline happy. Apply the Google Java Formatter. modified:   src/test/aks/playwright-ui-test/src/main/java/com/microsoft/azure/javaee/FetchBuildVersion.java
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- src/test/aks/playwright-ui-test/src/main/java/com/microsoft/azure/javaee/FetchBuildVersion.java
+++ src/test/aks/playwright-ui-test/src/main/java/com/microsoft/azure/javaee/FetchBuildVersion.java
@@ -1,25 +1,21 @@
 package com.microsoft.azure.javaee;
 
 import com.microsoft.playwright.*;
-/**
- * Hello world!
- *
- */
-public class FetchBuildVersion 
-{
-    public static void main( String[] args )
-    {
-        try (Playwright playwright = Playwright.create()) {
-            BrowserType webkit = playwright.webkit();
-            Browser browser = webkit.launch(new BrowserType.LaunchOptions().setHeadless(true));
-            Page page = browser.newPage();
-            page.navigate(args[0]);
 
-            ElementHandle p = page.querySelector("xpath=/html/body/div[1]/div/div[2]/p");
+/** Hello world! */
+public class FetchBuildVersion {
+  public static void main(String[] args) {
+    try (Playwright playwright = Playwright.create()) {
+      BrowserType webkit = playwright.webkit();
+      Browser browser = webkit.launch(new BrowserType.LaunchOptions().setHeadless(true));
+      Page page = browser.newPage();
+      page.navigate(args[0]);
 
-            if(!p.innerText().equals(args[1])) {
-                throw new RuntimeException("Build Version is not updated!");
-            }
-        }
+      ElementHandle p = page.querySelector("xpath=/html/body/div[1]/div/div[2]/p");
+
+      if (!p.innerText().equals(args[1])) {
+        throw new RuntimeException("Build Version is not updated!");
+      }
     }
+  }
 }
