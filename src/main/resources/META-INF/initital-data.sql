420a6820d8041bbd39e70dec48558960acb04db7
Chanun Ong <ocseal@gmail.com>
2022-06-22 15:11:06 -0700

    Initial-data.sql remove comment

--- src/main/resources/META-INF/initital-data.sql
+++ src/main/resources/META-INF/initital-data.sql
@@ -1,1 +1,1 @@
-INSERT INTO applicationsettings (select 1 as id, 0 as sampleloaded from applicationsettings where id = 1 having count(*)=0)
+INSERT INTO applicationsettings (id, sampleloaded) VALUES (1, FALSE);
