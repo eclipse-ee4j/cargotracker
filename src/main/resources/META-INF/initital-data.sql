ed137b3f9f7c1c320bf6d3a51433563ac36255f9
Chanun Ong <ocseal@gmail.com>
2022-06-22 14:27:32 -0700

    HSQL DB update

--- src/main/resources/META-INF/initital-data.sql
+++ src/main/resources/META-INF/initital-data.sql
@@ -1,2 +1,1 @@
--- It's OK for this to fail if the record already exists.
 INSERT INTO applicationsettings (select 1 as id, 0 as sampleloaded from applicationsettings where id = 1 having count(*)=0)
