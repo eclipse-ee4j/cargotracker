2b110093709c897afad2d6e75be48f124a40e3a8
hantsy <hantsy@gmail.com>
2023-01-02 15:38:42 +0800

    chore: remove version in FacesConfiguration

--- src/main/java/org/eclipse/cargotracker/interfaces/FacesConfiguration.java
+++ src/main/java/org/eclipse/cargotracker/interfaces/FacesConfiguration.java
@@ -2,9 +2,8 @@ package org.eclipse.cargotracker.interfaces;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.annotation.FacesConfig;
-import jakarta.faces.annotation.FacesConfig.Version;
 
 /** Jakarta Faces configuration. * */
-@FacesConfig(version = Version.JSF_2_3)
+@FacesConfig()
 @ApplicationScoped
 public class FacesConfiguration {}
