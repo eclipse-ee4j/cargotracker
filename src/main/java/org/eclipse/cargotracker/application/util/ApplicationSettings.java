28354cc1fc7c77b38261f76310c5f3dc1bf19667
David Matějček <david.matejcek@omnifish.ee>
2023-03-23 12:56:13 +0100

    Workaround for the "SMALLINT" issue in EclipseLink/GF/Derby type mapping
    - Resolves "boolean cannot fit to boolean column" error
    - I will try to fix the cause, then the columnDefinition can be removed
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- src/main/java/org/eclipse/cargotracker/application/util/ApplicationSettings.java
+++ src/main/java/org/eclipse/cargotracker/application/util/ApplicationSettings.java
@@ -1,7 +1,9 @@
 package org.eclipse.cargotracker.application.util;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
 /**
@@ -12,9 +14,21 @@ import java.io.Serializable;
 public class ApplicationSettings implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  @Id private Long id;
+  @Id
+  private long id;
 
+  // Fixes EclipseLink 4.0.1 mapping to Derby 10.15.2.0
+  // Note that this table is not used in tests!!!
+  @Column(columnDefinition = "boolean not null")
   private boolean sampleLoaded = false;
+
+  public ApplicationSettings(long id) {
+      this.id = id;
+  }
+
+  public ApplicationSettings() {
+
+  }
 
   public boolean isSampleLoaded() {
     return sampleLoaded;
