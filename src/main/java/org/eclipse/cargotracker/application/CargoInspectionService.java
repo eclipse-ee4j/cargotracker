0c26ba318a39a4939fadc3c59fdf032a5bab0003
Reza Rahman <reza_rahman@lycos.com>
2023-03-25 15:18:11 -0400

    Standardize formatting

--- src/main/java/org/eclipse/cargotracker/application/CargoInspectionService.java
+++ src/main/java/org/eclipse/cargotracker/application/CargoInspectionService.java
@@ -2,6 +2,7 @@ package org.eclipse.cargotracker.application;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
 import org.eclipse.cargotracker.domain.model.cargo.TrackingId;
 
 public interface CargoInspectionService {
